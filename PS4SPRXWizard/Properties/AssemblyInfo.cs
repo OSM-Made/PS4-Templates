0bb4975fbbe9380be24cc8b09bb5ce5d4294920c
OSM-Made <oldskoolmodz@gmail.com>
2025-12-05 20:10:22 -0700

    Add project files.

---
+++ PS4SPRXWizard/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PS4SPRXWizard")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PS4SPRXWizard")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30a14afc-f989-4a5e-995c-23e59cac6134")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PS4SPRXWizard/ScriptCopyWizard.cs
@@ -0,0 +1,48 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PS4SPRXWizard
+{
+    public class ScriptCopyWizard : IWizard
+    {
+        private string _solutionDir;
+        private string _templateDir;
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            _solutionDir = replacementsDictionary["$solutiondirectory$"];
+            _templateDir = Path.GetDirectoryName((string)customParams[0]);
+        }
+
+        public void RunFinished()
+        {
+            try
+            {
+                // Create External folder
+                Directory.CreateDirectory(Path.Combine(_solutionDir, "External"));
+
+                // Copy Scripts folder
+                string sourceScripts = Path.Combine(_templateDir, "Scripts");
+                string destScripts = Path.Combine(_solutionDir, "Scripts");
+
+                if (Directory.Exists(sourceScripts))
+                {
+                    Directory.CreateDirectory(destScripts);
+                    foreach (string file in Directory.GetFiles(sourceScripts))
+                    {
+                        File.Copy(file, Path.Combine(destScripts, Path.GetFileName(file)), true);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        public void ProjectFinishedGenerating(Project project) { }
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem) { }
+        public void BeforeOpeningFile(ProjectItem projectItem) { }
+        public bool ShouldAddProjectItem(string filePath) => true;
+    }
+}
